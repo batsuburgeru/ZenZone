c68e23217117378d64523793a0b54047b173d16c
Joshua Miguel Bernal <joshua_bernal@outlook.com>
2024-07-08 20:55:00 +0800

    updated Java and XML codes

--- app/src/main/java/com/example/cpe408finalproject/adminDashboard.java
+++ app/src/main/java/com/example/cpe408finalproject/adminDashboard.java
@@ -1,14 +1,31 @@
 package com.example.cpe408finalproject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class adminDashboard extends AppCompatActivity {
+
+    private TextView nameGreeting;
+    private EditText addUsername;
+    private EditText addPassword;
+    private Button addSubmit;
+    private EditText updateCurrentUsername;
+    private EditText updateNewUsername;
+    private EditText updateNewPass;
+    private Button updateSubmit;
+    private EditText delUser;
+    private Button delSubmit;
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,29 @@ public class adminDashboard extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        nameGreeting = findViewById(R.id.nameGreetingText);
+        addUsername = findViewById(R.id.usernameInput);
+        addPassword = findViewById(R.id.passwordInput);
+        addSubmit = findViewById(R.id.addUserBtn);
+        updateCurrentUsername = findViewById(R.id.currentUsernameInput);
+        updateNewUsername = findViewById(R.id.newUsernameInput);
+        updateNewPass = findViewById(R.id.newPassInput);
+        updateSubmit = findViewById(R.id.updateUserBtn);
+        delUser = findViewById(R.id.delUserInput);
+        delSubmit = findViewById(R.id.delUserBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+    }
+    private void showDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(adminDashboard.this);
+        builder.setMessage(message).setCancelable(false).setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
--- app/src/main/java/com/example/cpe408finalproject/superAdminDashboard.java
+++ app/src/main/java/com/example/cpe408finalproject/superAdminDashboard.java
@@ -1,14 +1,34 @@
 package com.example.cpe408finalproject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class superAdminDashboard extends AppCompatActivity {
+
+    private TextView nameGreeting;
+    private EditText addUsername;
+    private EditText addPassword;
+    private RadioGroup userTypeRad;
+    private Button addSubmit;
+    private EditText updateCurrentUsername;
+    private EditText updateNewUsername;
+    private EditText updateNewPass;
+    private Button updateSubmit;
+    private EditText delUser;
+    private Button delSubmit;
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,29 @@ public class superAdminDashboard extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nameGreeting = findViewById(R.id.nameGreetingText);
+        addUsername = findViewById(R.id.usernameInput);
+        addPassword = findViewById(R.id.passwordInput);
+        userTypeRad = findViewById(R.id.userTypeRadGroup);
+        addSubmit = findViewById(R.id.addUserBtn);
+        updateCurrentUsername = findViewById(R.id.currentUsernameInput);
+        updateNewUsername = findViewById(R.id.newUsernameInput);
+        updateNewPass = findViewById(R.id.newPassInput);
+        updateSubmit = findViewById(R.id.updateUserBtn);
+        delUser = findViewById(R.id.delUserInput);
+        delSubmit = findViewById(R.id.delUserBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+    }
+    private void showDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(superAdminDashboard.this);
+        builder.setMessage(message).setCancelable(false).setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
