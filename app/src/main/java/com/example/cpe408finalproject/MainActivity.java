d34d9e7a3514694ae0d9f51957ff03c54c13dc41
Alex <batoalexander10@gmail.com>
2024-07-08 19:53:53 +0800

    login page without background

---
+++ app/src/main/java/com/example/cpe408finalproject/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.cpe408finalproject;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText loginInputUsername;
+    private EditText loginInputPassword;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        loginInputUsername = findViewById(R.id.loginInputUsername);
+        loginInputPassword = findViewById(R.id.loginInputPassword);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String username = loginInputUsername.getText().toString();
+                String password = loginInputPassword.getText().toString();
+
+                if (username.isEmpty()) {
+                    showDialog("Username Required!");
+                }
+                else if (password.isEmpty()) {
+                    showDialog("Password Required!");
+                }
+                else {
+                    showDialog("Welcome!");
+                }
+            }
+        });
+    }
+
+    private void showDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(message).setCancelable(false).setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
