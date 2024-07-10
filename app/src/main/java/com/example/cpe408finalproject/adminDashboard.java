3844fc7a7c5190b48103f4f90feacd1fe250b34c
TadsueE <jthads55@gmail.com>
2024-07-10 11:33:54 +0800

    Admin buttons done

--- app/src/main/java/com/example/cpe408finalproject/adminDashboard.java
+++ app/src/main/java/com/example/cpe408finalproject/adminDashboard.java
@@ -1,22 +1,39 @@
 package com.example.cpe408finalproject;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class adminDashboard extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private TextView nameGreeting;
     private EditText addUsername;
     private EditText addPassword;
@@ -41,7 +58,7 @@ public class adminDashboard extends AppCompatActivity {
         });
 
         nameGreeting = findViewById(R.id.nameGreetingText);
-        addUsername = findViewById(R.id.newUsernameInput);
+        addUsername = findViewById(R.id.usernameInput);
         addPassword = findViewById(R.id.passwordInput);
         addSubmit = findViewById(R.id.addUserBtn);
         updateCurrentUsername = findViewById(R.id.currentUsernameInput);
@@ -57,18 +74,33 @@ public class adminDashboard extends AppCompatActivity {
             public void onClick(View view) {
                 String username = addUsername.getText().toString();
                 String password = addPassword.getText().toString();
+                String role = "user";
 
                 if (username.isEmpty() || password.isEmpty()) {
                     showDialog("Please enter the required information.");
                 }
                 else {
-                    showDialog("Success! User added.");
-                    addUsername.setText("");
-                    addPassword.setText("");
-                }
-            }
-        });
-
+                    Map<String,Object> users = new HashMap<>();
+                    users.put("username", username);
+                    users.put("password", password);
+                    users.put("role", role);
+                    db.collection("users")
+                            .whereEqualTo("username", username)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()){
+                                        if (task.getResult().isEmpty()){
+                                            db.collection("users")
+                                                    .add(users);
+                                            Log.w(TAG, "User added");
+                                            showDialog("User has been added successfully");
+                                        } else { showDialog("Username has already been taken");}
+                                    }
+                                }
+                            });
+        }}});
         updateSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,10 +112,43 @@ public class adminDashboard extends AppCompatActivity {
                     showDialog("Please enter the required information.");
                 }
                 else {
-                    showDialog("Success! User information updated.");
-                    updateCurrentUsername.setText("");
-                    updateNewUsername.setText("");
-                    updateNewPass.setText("");
+                    db.collection("users")
+                            .whereEqualTo("username", currentUser)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        if(task.getResult().isEmpty()){
+                                            Log.w(TAG, "Updating Failed");
+                                            showDialog("User does not exist");
+                                        }
+                                        else
+                                        {
+                                            DocumentSnapshot document = task.getResult().getDocuments().get(0);
+                                            DocumentReference documentReference = document.getReference();
+                                            Map<String,Object> users = new HashMap<>();
+                                            users.put("username", newUser);
+                                            users.put("password", password);
+                                            documentReference.update(users)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.w(TAG, "Updated user");
+                                                        showDialog("Success! User information updated.");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(TAG, "Updated Failed");
+                                                        showDialog("Failed! User information not updated.");
+                                                    }
+                                                });}
+                                    } else { showDialog("User Information Error");}
+                                }
+                            });
+
                 }
             }
         });
@@ -97,16 +162,46 @@ public class adminDashboard extends AppCompatActivity {
                     showDialog("Username cannot be blank!");
                 }
                 else {
-                    showDialog("Success! User " + username+ " successfully deleted.");
-                    delUser.setText("");
-                }
-            }
-        });
+                    db.collection("users")
+                            .whereEqualTo("username", username)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()){
+                                        if (task.getResult().isEmpty()){
+                                            Log.w(TAG, "User nonexistent");
+                                            showDialog("User does not exist");
+                                        }
+                                        else {
+                                            DocumentSnapshot document = task.getResult().getDocuments().get(0);
+                                            DocumentReference documentReference = document.getReference();
+                                            documentReference.delete()
+                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                        @Override
+                                                        public void onSuccess(Void unused) {
+                                                            Log.w(TAG, "Delete success");
+                                                            showDialog("Success! User " + username+ " successfully deleted.");
+                                                        }
+                                                    })
+                                                    .addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Log.w(TAG, "Delete Failed");
+                                                            showDialog("Deletion unsuccessful");
+                                                        }
+                                                    });
+                                        }
+                                    } else { Log.w(TAG, "Unknown Error"); showDialog("Error Detected");}
+                                }
+                            });
+                }}});
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(adminDashboard.this, MainActivity.class));
+                finish();
             }
         });
     }
