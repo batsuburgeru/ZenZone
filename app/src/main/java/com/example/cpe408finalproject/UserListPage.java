342e02476b7cd3a40d2a0fb8e63c99e327aff874
Alex <batoalexander10@gmail.com>
2024-07-10 13:05:57 +0800

    chore: add routing to UserListPage

--- app/src/main/java/com/example/cpe408finalproject/UserListPage.java
+++ app/src/main/java/com/example/cpe408finalproject/UserListPage.java
@@ -1,6 +1,10 @@
 package com.example.cpe408finalproject;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class UserListPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,28 @@ public class UserListPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button userNextButton = findViewById(R.id.userNextButton);
+
+        TableLayout tableLayout = findViewById(R.id.tableLayout);
+
+        JSONObject userData = new JSONObject();
+        try {
+            userData.put("username", "usernameTest");
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Log.d("test", String.valueOf(userData));
+
+        userNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 }
